a80bb0ed9d507fc1fa4bc862847724b24fe127e3
Bryce Hill <bryceahill@gmail.com>
2015-10-03 13:24:24 -0700

    Add favicon

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
+var favicon = require('serve-favicon')
 var app = express()
 
 app.use(express.static('public'))
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
