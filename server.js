acd9067d479245c4d912cab9144f1a222c1cb0fb
Bryce Hill <bryceahill@gmail.com>
2015-10-10 15:45:41 -0700

    Add handlerbars templating

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express')
+var exphbs  = require('express-handlebars')
 var favicon = require('serve-favicon')
 var app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('home')
+})
 })
 
 var server = app.listen(3000, function () {
