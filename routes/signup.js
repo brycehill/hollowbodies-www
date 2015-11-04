a0e231b65042ca643535bab8569c9cf5dd78f44f
Bryce Hill <bryceahill@gmail.com>
2015-11-03 21:03:54 -0700

    Validate email on server

--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,15 @@
 var config = require('../config')
+var validator = require('email-validator')
 var MC = require('mailchimp-api').Mailchimp
 var mc = new MC(config.mailchimp.key)
 
 // @todo Validate email. Geo Ip?
 exports.post = function(req, res) {
     var email = req.body.email
+
+    if (!validator.validate(email)) {
+        return res.status(400).json({ msg: '' })
+    }
 
     mc.lists.subscribe({
         id: config.mailchimp.list.id,
