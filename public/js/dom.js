ba8e1e140e443fd4ab3216478e95a2ce45fbb7bd
Bryce Hill <bryceahill@gmail.com>
2015-11-03 21:39:32 -0700

    Add loading state

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,14 +1,18 @@
 import {email as isEmail} from './validate'
 import signup from './signup'
 
+// @todo: Make all this pure and stuff. I wanted to get it done first.
+
 var $form = document.querySelector('#signup')
 $form.addEventListener('submit', e => {
     e.preventDefault()
 
     var addy = e.target.children.email.value
     if (isEmail(addy)) {
+        document.querySelector('#loader').removeAttribute('hidden')
         signup(addy).fork(handleAPIError, function(res) {
             showMessage('Thanks! Don\'t forget to check your email to confirm your subscription.')
+            document.querySelector('#loader').setAttribute('hidden', 'hidden')
         })
     } else {
         showError('Whoops, that doesn\'t look like a valid email address. Try again?')
@@ -35,6 +39,7 @@ function handleAPIError(response) {
             'Unfortunately, you will have to manually subscribe to this mailing list. Thanks for understanding',
         'List_AlreadySubscribed': 'Looks like this email has already subscribed to this list. Thanks for subscribing!'
     }
+    document.querySelector('#loader').setAttribute('hidden', 'hidden')
     showError(rs[response.name]);
 }
 
